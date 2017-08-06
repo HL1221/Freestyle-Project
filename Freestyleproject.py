a34c72dee654ca82e5338e03c2685cb6173f80ee
Harold Lander <dudew1000@Harold-Landers-MacBook-Pro.local>
2017-08-06 18:23:27 -0400

    final project

--- Freestyleproject.py
+++ Freestyleproject.py
@@ -12,32 +12,11 @@ def histreturn ():
     start_day = int(input ("Start Day of Quote: "))
     start = datetime.datetime(start_year, start_month, start_day)
 
-    todaytest = str(input("Would you like to end the search today? (Y/N)"))
-    todaytest = todaytest.lower()
-    if todaytest == "y":
-        end = date.today()
-    else:
-        end_year = int(input ("Ending Year of Quote: "))
-        end_month = int(input ("Ending Month of Quote: "))
-        end_day = int(input ("Ending Day of Quote: "))
-        end = datetime.datetime(end_year, end_month, end_day)
+    end_month = int(input ("Ending Month of Quote: "))
+    end_day = int(input ("Ending Day of Quote: "))
+    end = datetime.datetime(end_year, end_month, end_day)
 
     lookup = web.DataReader(symbol, 'yahoo', start, end)
-
-    print(lookup)
-
-    Specific_data_test = str(input("Would you like to save to CSV? (Y/N)"))
-    Specific_data_test = Specific_data_test.lower()
-
-    if Specific_data_test == "n":
-        print("Thank you for your search")
-    else:
-        other_path = "Single Day Return.csv"
-        with open(other_path, "w") as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=["Date","Open","high","Low","Close","Adj Close","Volume"])
-            writer.writeheader() # uses fieldnames set above
-            for date in lookup:
-                writer.writerow(date)
 
 
 def div_Split ():
@@ -46,48 +25,62 @@ def div_Split ():
     start_day = int(input ("Start Day of Quote: "))
     start = datetime.datetime(start_year, start_month, start_day)
 
-    todaytest = str(input("Would you like to end the search today? (Y/N)"))
-    todaytest = todaytest.lower()
-    if todaytest == "y":
-        end = date.today()
-    else:
-        end_year = int(input ("Ending Year of Quote: "))
-        end_month = int(input ("Ending Month of Quote: "))
-        end_day = int(input ("Ending Day of Quote: "))
-        end = datetime.datetime(end_year, end_month, end_day)
+    end_month = int(input ("Ending Month of Quote: "))
+    end_day = int(input ("Ending Day of Quote: "))
+    end = datetime.datetime(end_year, end_month, end_day)
 
     lookup = web.DataReader(symbol, 'yahoo-dividends', start, end)
     print(lookup)
 
-def daily_close ():
-    start = str(date.today() - timedelta(days=15)) #> '2017-07-09'
-    end = str(date.today())
-    lookup = web.DataReader(symbol, 'yahoo', start, end)
-    print (lookup)
 
-# print("""
-# Available Functions:
-# Single Day Return               (Single)
-# Dividends and date of issuance  (Div)
-# List of Daily Closes            (Close)
-# """)
-#
-# ActFunction = input("Please Specify which function you want: ")
-# ActFunction = ActFunction.lower()
-# symbol = str(input("Please enter the stock ticker: "))
-# symbol = symbol.upper()
+def compare_stocks ():
+    stocks_to_compare = int(input("How many stocks would you like to compare? "))
+
+    stocks_symbols = list()
+
+    start_year = int(input ("Start Year of Quote: "))
+    start_month = int(input ("Start Month of Quote: "))
+    start_day = int(input ("Start Day of Quote: "))
+    start = datetime.datetime(start_year, start_month, start_day)
+
+    end_month = int(input ("Ending Month of Quote: "))
+    end_day = int(input ("Ending Day of Quote: "))
+    end = datetime.datetime(end_year, end_month, end_day)
+
+    for i in range(int(stocks_to_compare)):
+        user_stock_symbols = str(input("Please enter a stock symbol "))
+        user_stock_symbols = user_stock_symbols.upper()
+
+        stocks_symbols.append(str(user_stock_symbols))
+
+    lookup = web.DataReader(stocks_symbols, 'yahoo', start, end)
+
+    daily_closing_prices = lookup.ix["Close"]
+
+    print(daily_closing_prices)
+
+
+print("""
+Available Functions:
+Single Day Return               (Single)
+Dividends and date of issuance  (Div)
+Compare two stocks              (Compare)
+""")
+
+ActFunction = input("Please Specify which function you want: ")
+ActFunction = ActFunction.lower()
+symbol = str(input("Please enter the stock ticker: "))
+symbol = symbol.upper()
 
 start = []
 end = []
 
-ActFunction = "div"
-symbol = 'AAPL'
 
 if ActFunction == "single":
     histreturn ()
 elif ActFunction == "div":
     div_Split ()
-elif ActFunction == "close":
-    daily_close ()
+elif ActFunction == "compare":
+    compare_stocks ()
 else:
     print("Done")
